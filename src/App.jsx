e474b031eb6f943b0480549c64f90ecc86179fba
Ali Saif <145758021+AliSaif1@users.noreply.github.com>
2025-06-26 22:27:56 +0500

    Fixed Footer path

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import About from "./pages/About"
 import Services from "./pages/Services"
 // import Contact from "./pages/Contact"
 // import Portfolio from "./pages/Portfolio"
-import Footer from "./components/footer"
+import Footer from "./components/Footer"
 
 export default function App() {
   return (
